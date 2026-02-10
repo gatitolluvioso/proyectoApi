f788dcca50acc56fab37338b98a963630afe0eec
gatitolluvioso <davidabrahamope@gmail.com>
2026-02-10 17:37:45 -0600

    se modifico la funcion extraerDatos para traer todos los drinks y no buscarlos letra por letra, cambios minimos a la interfaz y se agrego el footer con chistes malos

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-datos = {};
+datos = {
+  drinks: [],
+};
 
-function extraerDatos() {
-  fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a")
-    .then((response) => response.json())
-    .then((data) => {
-      datos = data;
-      console.log(datos);
-      mostrarDatos();
-    });
+async function extraerDatos() {
+  document.getElementById("tarjetas").innerHTML = "<p>Cargando drinks...</p>";
+  const letras = "abcdefghijklmnopqrstuvwxyz";
+
+  for (let letra of letras) {
+    const response = await fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letra}`,
+    );
+    const data = await response.json();
+
+    if (data.drinks) {
+      datos.drinks = datos.drinks.concat(data.drinks);
+    }
+  }
+  console.log("Total de drinks: ", datos.drinks.length);
+  mostrarDatos();
+  mostrarChistes();
 }
 
 function mostrarDatos() {
   let tDrinks = document.getElementById("tarjetas");
+  tDrinks.innerHTML = "";
   for (let i = 0; i < datos.drinks.length; i++) {
     var tarjeta = document.createElement("div");
     var tragoNombre = document.createElement("h2");
@@ -24,4 +36,17 @@ function mostrarDatos() {
     tarjeta.appendChild(tragoImagen);
     tDrinks.appendChild(tarjeta);
   }
+}
+function mostrarChistes() {
+  var chistesukis = {};
+  let chistes = document.getElementById("chistes");
+  fetch(
+    "https://official-joke-api.appspot.com/random_joke?ref=freepublicapis.com",
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const chiste = document.createElement("p");
+      chiste.textContent = data.setup + " - " + data.punchline;
+      chistes.appendChild(chiste);
+    });
 }
